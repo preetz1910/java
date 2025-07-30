f0eb07d64be44621fe6f1b4bfd2c3bc6b7221f0c
preetz1910 <preetha.240231@aids.ritchennai.edu.in>
2025-07-30 20:44:55 +0530

    Create project1.java

---
+++ project1.java
@@ -0,0 +1,9 @@
+public class SumHundred {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
+        }
+        System.out.println("The sum of all numbers from 1 to 100 is: " + sum);
+    }
+}
